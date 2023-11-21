1414c6b197b1b14829abc35ff0dfaa2653330788
Fernando Roa <froao@unal.edu.co>
2023-11-21 18:01:15 -0300

    feat: exchange functions

---
+++ R/shinyInvoice-package.R
@@ -0,0 +1,8 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rlang :=
+## usethis namespace: end
+NULL
+utils::globalVariables(c(".data", "date_Sy", "date_input", "avg_low_high_this_fun_Sy"))
